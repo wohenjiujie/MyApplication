69c130a1391ed72a2c80940a5ce550596f95b159
hasee <2826001553@qq.com>
2020-01-27 15:29:36 +0800

    密码找回的布局，activity

--- app/src/main/java/com/organization/myapplication/ForgetActivity.java
+++ app/src/main/java/com/organization/myapplication/ForgetActivity.java
@@ -1,11 +1,14 @@
 package com.organization.myapplication;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 /*
 * 忘记密码activity
 * */
 public class ForgetActivity extends BaseActivity {
+    private TextView by_psw,by_phone,by_protect;
+
     @Override
     protected void runOnProcess(Bundle bundle) {
 
@@ -18,6 +21,7 @@ public class ForgetActivity extends BaseActivity {
 
     @Override
     protected int getContentViewId() {
-        return R.layout.activity_forget;
+        return R.layout.forget_activity;
     }
+
 }
