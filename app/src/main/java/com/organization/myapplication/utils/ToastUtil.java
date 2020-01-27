1597243e1a80489dc6cd1f03c5e0098c31820fd3
hasee <2826001553@qq.com>
2020-01-27 15:29:21 +0800

    Merge branch 'master' of D:\java-idea\MyApplication with conflicts.

--- app/src/main/java/com/organization/myapplication/utils/ToastUtil.java
+++ app/src/main/java/com/organization/myapplication/utils/ToastUtil.java
@@ -5,14 +5,13 @@ import android.widget.Toast;
 
 public class ToastUtil {
     private Context c;
-    private String s;
     ToastUtil(Context context){
         c=context;
     }
-    void shorttoast(String s){
-        Toast.makeText(c,s,Toast.LENGTH_SHORT).show();
+    void shorttoast(String text){
+        Toast.makeText(c,text,Toast.LENGTH_SHORT).show();
     }
-    public void longtoast(String s){
-        Toast.makeText(c,s,Toast.LENGTH_LONG).show();
+    public void longtoast(String text){
+        Toast.makeText(c,text,Toast.LENGTH_LONG).show();
     }
 }
